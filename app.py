b4b704fc20feda70725aee9fcaebbbd65b7c9d65
tatosec <ivang.fonsecafrias@gmail.com>
2023-10-20 13:52:48 -0400

    Fixed Listener

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, jsonify
 import subprocess
+import threading
+import time
 
 app = Flask(__name__)
 
@@ -18,7 +20,6 @@ def start_netcat_listener():
                 pass
 
 # Start the Netcat listener in a separate thread
-import threading
 listener_thread = threading.Thread(target=start_netcat_listener)
 listener_thread.start()
 
@@ -32,12 +33,20 @@ def execute():
     if request.method == 'POST':
         command = request.form['command']
         try:
-            result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-            if connection_established:
-                response = "[+] Connection Established\n" + result.decode()
+            if command == "nc -lvnp 4444":
+                if not connection_established:
+                    subprocess.Popen(["nc", "-lvnp", "4444"])
+                    time.sleep(1)  # Give time for the listener to start
+                    connection_established = True
+                else:
+                    return jsonify({'output': 'Listener is already running.'})
             else:
-                response = result.decode()
-            return jsonify({'output': response})
+                result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+                if connection_established:
+                    response = "[+] Connection Established\n" + result.decode()
+                else:
+                    response = result.decode()
+                return jsonify({'output': response})
         except subprocess.CalledProcessError as e:
             return jsonify({'output': str(e.output)})
 
