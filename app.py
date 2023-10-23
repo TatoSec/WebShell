219ae6ccff715d31e807cc5e989abeac047c23ef
tatosec <ivang.fonsecafrias@gmail.com>
2023-10-23 09:41:16 -0400

    Fixed Connection Issues

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, jsonify
+
 import subprocess
 import threading
 import time
@@ -7,6 +8,7 @@ app = Flask(__name__)
 
 # Flag to track if a connection has been established
 connection_established = False
+listener_thread = None
 
 # Function to run the Netcat listener
 def start_netcat_listener():
@@ -19,37 +21,34 @@ def start_netcat_listener():
             except subprocess.CalledProcessError:
                 pass
 
-# Start the Netcat listener in a separate thread
-listener_thread = threading.Thread(target=start_netcat_listener)
-listener_thread.start()
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/execute', methods=['POST'])
 def execute():
-    global connection_established
+    global connection_established, listener_thread
     if request.method == 'POST':
         command = request.form['command']
-        try:
-            if command == "nc -lvnp 5757":
-                if not connection_established:
-                    subprocess.Popen(["nc", "-lvnp", "5757"])
-                    time.sleep(1)  # Give time for the listener to start
-                    connection_established = True
-                else:
-                    return jsonify({'output': 'Listener is already running.'})
+        if command == "listen":
+            if not connection_established:
+                listener_thread = threading.Thread(target=start_netcat_listener)
+                listener_thread.daemon = True
+                listener_thread.start()
+                connection_established = True
+                return jsonify({'output': 'Listener started successfully.'})
             else:
+                return jsonify({'output': 'Listener is already running.'})
+        else:
+            try:
                 result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
                 if connection_established:
                     response = "[+] Connection Established\n" + result.decode()
                 else:
                     response = result.decode()
                 return jsonify({'output': response})
-        except subprocess.CalledProcessError as e:
-            return jsonify({'output': str(e.output)})
+            except subprocess.CalledProcessError as e:
+                return jsonify({'output': str(e.output)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-
